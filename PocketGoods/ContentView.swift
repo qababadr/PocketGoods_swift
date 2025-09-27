57096df30fe9b1f726ba22204be494dd3648d95d
badr qaba <qababadr@gmail.com>
2025-09-27 19:41:02 +0100

    project setup

---
+++ PocketGoods/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PocketGoods
+//
+//  Created by BADR  QABA on 2025-09-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PocketGoods/PocketGoodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PocketGoodsApp.swift
+//  PocketGoods
+//
+//  Created by BADR  QABA on 2025-09-27.
+//
+
+import SwiftUI
+
+@main
+struct PocketGoodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
